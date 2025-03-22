2121538240d60922c173f12cdb054f8f1eb29d17
iki789 <ikhlak789@gmail.com>
2025-03-21 18:42:48 -0700

    added editor-ui

--- frontend/src/renderer/Components/Editor/index.tsx
+++ frontend/src/renderer/Components/Editor/index.tsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import EditorJS, { OutputData } from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+// import Slate from './Slate';
 
-function Editor() {
-  return <div>Editor</div>;
+// CSS styles
+// import "./styles.css";
+
+interface EditorProps {
+  initialData: OutputData;
+}
+
+function Editor(props: EditorProps) {
+  const { initialData } = props;
+  const editorRef = useRef<EditorJS | null>(null);
+
+  const handleSave = async () => {
+    if (editorRef.current) {
+      const outputData: OutputData = await editorRef.current.save();
+      console.log('Editor Content:', outputData);
+    }
+  };
+
+  useEffect(() => {
+    // Initialize EditorJS
+    editorRef.current = new EditorJS({
+      holder: 'editor-container',
+      autofocus: true,
+      tools: {
+        header: Header,
+      },
+      defaultBlock: 'header',
+      onChange: (e) => {
+        console.log(e);
+      },
+      data: initialData,
+    });
+
+    // Cleanup function to destroy the editor when the component unmounts
+    return () => {
+      if (editorRef.current) {
+        editorRef.current.destroy();
+      }
+    };
+  }, [initialData]);
+
+  return (
+    <div className="w-full h-full relative">
+      <div id="editor-container" className="w-full h-full" />
+    </div>
+  );
+
+  // return <Slate />;
 }
 
 export default Editor;
