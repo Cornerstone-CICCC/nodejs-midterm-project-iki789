d1b317611436d51d5332b8adc5218637804d4555
iki789 <ikhlak789@gmail.com>
2025-03-22 04:39:19 -0700

    fixed producion build css

--- frontend/.erb/configs/webpack.config.renderer.prod.ts
+++ frontend/.erb/configs/webpack.config.renderer.prod.ts
@@ -39,24 +39,8 @@ const configuration: webpack.Configuration = {
   module: {
     rules: [
       {
-        test: /\.s?(a|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: true,
-              importLoaders: 1,
-            },
-          },
-          'sass-loader',
-        ],
-        include: /\.module\.s?(c|a)ss$/,
-      },
-      {
-        test: /\.s?(a|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'], // add "postcss-loader" to CSS loaders
         exclude: /\.module\.s?(c|a)ss$/,
       },
       // Fonts
